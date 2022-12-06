22e65d64809850467a2434631747af37152ee3d6
Kosinee <107994771+Kosinee@users.noreply.github.com>
2022-12-07 00:31:44 +0300

    Create comment_tasks.py

---
+++ comment_tasks.py
@@ -0,0 +1,15 @@
+from pyrustools.client_plus import MyPyrus
+from pyrustools.object_methods import object_by_code
+
+def comment_task_before_consideration(task_number):
+    elem_MyPyrus = MyPyrus(login="reg+kostya@atlas-it-solutions.ru", security_key="cBDZ0tJgAIz4eO8TSXlY6laNnCPSxuiaXSyo1Rq-9pTmH2Eh9B-rIH044VRstjZmVQXvYP202ZxKQt2pTPd0SLTQD6qw00E1")
+    elem_MyPyrus.initialize()
+    task_getter = elem_MyPyrus.get_task(task_number)
+    elem_MyPyrus.update_task_field_info(task_getter.task)
+    defendant_name = object_by_code(task_getter.task.flat_fields, 'u_defendant_fio').value
+    cases = court_cases(defendant_name)
+    url_cases = court_cases_from_url(cases)
+    elem_MyPyrus.comment_task_plus(
+            task_id=task_number,
+            field_updates=initial_field_updates_form(cases, url_cases),
+            text='Мониторинг судебных заседаний')
