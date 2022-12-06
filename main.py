a6cd9942a428d487e0e5e2a456385ac9d5c0852a
Kosinee <107994771+Kosinee@users.noreply.github.com>
2022-12-07 00:28:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-case = csw.court_cases("Лепилин Денис Юрьевич")
-    url_case = csw.court_cases_from_url(case)
-    elem_MyPyrus = MyPyrus(login="reg+kostya@atlas-it-solutions.ru", security_key="cBDZ0tJgAIz4eO8TSXlY6laNnCPSxuiaXSyo1Rq-9pTmH2Eh9B-rIH044VRstjZmVQXvYP202ZxKQt2pTPd0SLTQD6qw00E1")
-    elem_MyPyrus.initialize()
-    elem_MyPyrus.comment_task_plus(
-            task_id=144050004,
-            field_updates=fup.initial_field_updates_form(case, url_case),
-            text='Мониторинг судебных заседаний')
+
+if __name__ == '__main__':
+    comment_task_before_consideration(144050106)
+    comment_task_before_consideration(144050004)
+    comment_task_before_consideration(143692151)
